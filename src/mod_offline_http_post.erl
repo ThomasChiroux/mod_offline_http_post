86e7553c698fae132e49bacd74f409a9b7a73dc3
Thomas Chiroux <thomas@chiroux.com>
2019-12-21 20:42:41 +0100

    still trying to make this module work. Lacks of ejabberd documentation updates is painfull

--- src/mod_offline_http_post.erl
+++ src/mod_offline_http_post.erl
@@ -33,9 +33,9 @@ mod_opt_type(confidential) ->
     econf:bool().
 
 mod_options(Host) ->
-    [{post_url, fun(Host) -> ejabberd_config:get_option({post_url, Host}) end},
-     {auth_token, fun(Host) -> ejabberd_config:get_option({auth_token, Host}) end},
-     {confidential, fun(Host) -> ejabberd_config:get_option({confidential, Host}) end}].
+    [{post_url, []},
+     {auth_token, []},
+     {confidential, []}].
 
 
 create_message({Action, Packet} = Acc) when (Packet#message.type == chat) and (Packet#message.body /= []) ->
@@ -54,12 +54,14 @@ create_message(_From, _To, Packet) when (Packet#message.type == chat) and (Packe
 
 post_offline_message(From, To, Body, MessageId) ->
   ?INFO_MSG("Posting From ~p To ~p Body ~p ID ~p~n",[From, To, Body, MessageId]),
-  Token = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, auth_token, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
-  PostUrl = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, post_url, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
+  Vhost = To#jid.lserver,
   ToUser = To#jid.luser,
   FromUser = From#jid.luser,
-  Vhost = To#jid.lserver,
-  case gen_mod:get_module_opt(To#jid.lserver, ?MODULE, confidential, false) of
+  Token = gen_mod:get_module_opt(Vhost, ?MODULE, auth_token),
+  PostUrl = gen_mod:get_module_opt(Vhost, ?MODULE, post_url),
+  %%Token = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, auth_token, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
+  %%PostUrl = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, post_url, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
+  case gen_mod:get_module_opt(Vhost, ?MODULE, confidential) of
     true -> Data = string:join(["to=", binary_to_list(ToUser), "&from=", binary_to_list(FromUser), "&vhost=", binary_to_list(Vhost), "&messageId=", binary_to_list(MessageId)], "");
     false -> Data = string:join(["to=", binary_to_list(ToUser), "&from=", binary_to_list(FromUser), "&vhost=", binary_to_list(Vhost), "&body=", binary_to_list(Body), "&messageId=", binary_to_list(MessageId)], "")
   end,
