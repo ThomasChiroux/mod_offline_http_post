cedf6516bd6052f0d7cf3636a92f1b68ddf5afd4
Thomas Chiroux <thomas@chiroux.com>
2019-12-21 20:59:18 +0100

    wip

--- src/mod_offline_http_post.erl
+++ src/mod_offline_http_post.erl
@@ -57,10 +57,10 @@ post_offline_message(From, To, Body, MessageId) ->
   Vhost = To#jid.lserver,
   ToUser = To#jid.luser,
   FromUser = From#jid.luser,
-  Token = gen_mod:get_module_opt(Vhost, ?MODULE, auth_token),
-  PostUrl = gen_mod:get_module_opt(Vhost, ?MODULE, post_url),
-  %%Token = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, auth_token, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
-  %%PostUrl = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, post_url, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
+  %%Token = gen_mod:get_module_opt(Vhost, ?MODULE, auth_token),
+  %%PostUrl = gen_mod:get_module_opt(Vhost, ?MODULE, post_url),
+  Token = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, auth_token, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
+  PostUrl = gen_mod:get_module_opt(To#jid.lserver, ?MODULE, post_url, fun(S) -> iolist_to_binary(S) end, list_to_binary("")),
   case gen_mod:get_module_opt(Vhost, ?MODULE, confidential) of
     true -> Data = string:join(["to=", binary_to_list(ToUser), "&from=", binary_to_list(FromUser), "&vhost=", binary_to_list(Vhost), "&messageId=", binary_to_list(MessageId)], "");
     false -> Data = string:join(["to=", binary_to_list(ToUser), "&from=", binary_to_list(FromUser), "&vhost=", binary_to_list(Vhost), "&body=", binary_to_list(Body), "&messageId=", binary_to_list(MessageId)], "")
